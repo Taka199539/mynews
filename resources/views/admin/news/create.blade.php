fd9a2a7e2a64d20059d0487d9e68814d67540995
Takayuki <taka19950309@gmail.com>
2020-10-25 04:21:06 +0000

    laravel_11_教材

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Comptible"
-        content="IE=edge">
-        <meta name="viewport" content="width=device-width,
-        initial-scale=1">
-        
-        <title>MyNews</title>
-        <body>
-            <h1>Myニュース作成画面</h1>
-        </body>
-    </head>
-</html>
+@extends('layouts.admin')
 
 
+@section('title','ニュースの新規作成')
+
+
+@section('content')
+     <div class="container">
+         <div class="row">
+             <div class="col-md-8 mx-auto">
+                 <h2>ニュース新規作成</h2>
+             </div>
+         </div>
+     </div>
+@endsection
 
