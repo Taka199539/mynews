efd5e5aee325a93f142f4a7882d54c187bacd2c2
Takayuki <taka19950309@gmail.com>
2020-10-27 09:13:12 +0000

    laravel_12_教材

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -31,7 +32,7 @@
                 <div class="container">
                     <a class="navbar-brand" href="{{ url('/') }}"> 
                     {{ config('app.mame','laravel')}} 
-                              </a>
+                    </a>
                     <button class="navbar-toggler" type="button" 
                     data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent"
@@ -48,6 +49,31 @@
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
                             
+                            @guest
+                           
+                           <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }} </a></li>
+                
+                            @else
+                            
+                           <li class="nav-item dropdown">
+                               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                               role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   {{ Auth::user()->name }} <span class="caret"></span>
+                               </a>
+                               
+                            <div class="dropdown-menu" aria-labelledy="navbarDropdown">
+                                   <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                       {{ __('Logout') }}
+                                   </a>
+                                   
+                                   <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: note;">
+                                       
+                                       @csrf
+                                   </form>
+                               </div>
+                           </li>
+                           @endguest
                         </ul>
                     </div>
                 </div>
