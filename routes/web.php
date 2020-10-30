daa5877deee6d22d774984ec0af3e044cdcbdae4
Takayuki <taka19950309@gmail.com>
2020-10-30 14:48:59 +0000

    laravel_15~16_教材

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,11 @@ Route::get('XXX','AAAControleer@bbb');
 admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てる*/
 Route::group(['prefix' => 'admin','middleware'=> 'auth'], function() {
     Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/edit','Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
     Route::post('news/create','Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/delete', 'Admin\NewsController@delete');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/create', 'Admin\ProfileController@create');
